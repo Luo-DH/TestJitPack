056a14a8fbc0ca633da896aded8d1bb9119e2d88
vansingluo <vansingluo@tencent.com>
2023-09-18 19:02:10 +0800

    7.2.1 ....

--- app/src/main/java/com/easy/testjitpack/MainActivity.kt
+++ app/src/main/java/com/easy/testjitpack/MainActivity.kt
@@ -2,14 +2,14 @@ package com.easy.testjitpack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.easy.mylibrary.Test
+//import com.easy.mylibrary.Test
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val a = Test.test
+//        val a = Test.test
 //        Test.testFrom2
     }
 }
